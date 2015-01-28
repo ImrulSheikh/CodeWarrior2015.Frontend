23c884a4f9542ddfb4b3c83a02891b7cef4d55f3
Tanjeer <tbh.tilok@live.com>
2015-01-28 22:54:02 +0600

    committed with login and sign up functionality

--- src/AppServer/Controllers/HomeController.cs
+++ src/AppServer/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Web.Mvc;
 
-namespace CW.Frontend.AppServer.Controllers {
-    public class HomeController : Controller {
-        public ActionResult Index() {
+namespace CW.Frontend.AppServer.Controllers 
+{
+    public class HomeController : Controller 
+    {
+        public ActionResult Index() 
+        {
             return View();
         }
     }
